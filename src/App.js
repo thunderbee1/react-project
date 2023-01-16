034875590587bb37cdd307a9b499b0af44606203
thunderbee1 <yuuetu1234@gmail.com>
2023-01-16 14:10:29 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Login Page
+      <div>
+        ID: <input></input>
+      </div>
+      <div>
+        PW: <input></input>
+      </div>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+git init
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:thunderbee1/react-project.git
+git push -u origin main
+*/
